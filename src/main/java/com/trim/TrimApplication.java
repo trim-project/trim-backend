63eb00743d47edfa4dc80485a1b3cb4df2015767
hamlsy <dltmddud1122@naver.com>
2024-11-02 23:17:19 +0900

    init: initial project setting

---
+++ src/main/java/com/trim/TrimApplication.java
@@ -0,0 +1,13 @@
+package com.trim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrimApplication.class, args);
+	}
+
+}
