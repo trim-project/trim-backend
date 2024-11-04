cbb119654f57245ba57edeb19f75e336a12220ac
hamlsy <dltmddud1122@naver.com>
2024-11-04 21:39:03 +0900

    chore(dependency): querydsl dependency 추가

--- src/main/java/com/trim/domain/question/repository/QuestionRepository.java
+++ src/main/java/com/trim/domain/question/repository/QuestionRepository.java
@@ -1,8 +1,12 @@
 package com.trim.domain.question.repository;
 
+import com.trim.domain.member.entity.Member;
 import com.trim.domain.question.entity.Question;
 import com.trim.domain.question.repository.querydsl.QuestionQueryRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Long>, QuestionQueryRepository {
+    List<Question> findByWriter(Member writer);
 }
--- src/main/java/com/trim/domain/question/repository/querydsl/QuestionQueryRepository.java
+++ src/main/java/com/trim/domain/question/repository/querydsl/QuestionQueryRepository.java
@@ -1,4 +1,9 @@
 package com.trim.domain.question.repository.querydsl;
 
+import com.trim.domain.question.entity.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface QuestionQueryRepository {
+//    Page<Question> searchByKeyword(QuestionSearchRequest request, Pageable pageable);
 }
