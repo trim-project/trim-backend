07811cc05af4bd519d2d8ac90f8481d4819c04ab
hamlsy <dltmddud1122@naver.com>
2024-11-08 00:30:17 +0900

    feat: Reason 클래스 작성 #3

---
+++ src/main/java/com/trim/exception/advice/ExceptionAdvice.java
@@ -0,0 +1,16 @@
+package com.trim.exception.advice;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice(annotations = {RestController.class})
+public class ExceptionAdvice extends ResponseEntityExceptionHandler {
+
+
+
+}
