4e71be46e89b6887a027c12a1668c268d5a17ca3
hamlsy <dltmddud1122@naver.com>
2024-11-08 14:41:27 +0900

    feat: add ApiResponseDto #3

---
+++ src/main/java/com/trim/exception/payload/dto/ApiResponseDto.java
@@ -0,0 +1,31 @@
+package com.trim.exception.payload.dto;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.trim.exception.payload.code.BaseCode;
+import com.trim.exception.payload.code.SuccessStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonPropertyOrder
+public class ApiResponseDto<T> {
+
+    private final Boolean isSuccess;
+    private final Integer code;
+    private final String message;
+    private T result;
+
+    public static <T> ApiResponseDto onSuccess(T result){
+        return new ApiResponseDto(true, 2000, SuccessStatus._SUCCESS.getMessage(), result);
+    }
+
+    public static <T> ApiResponseDto<T> of(BaseCode code, T result){
+        return new ApiResponseDto<>(true, code.getReasonHttpStatus().getCode(), code.getReason().getMessage(), result);
+    }
+
+    public static <T> ApiResponseDto<T> onFailure(Integer code, String message, T data){
+        return new ApiResponseDto<>(false, code, message, data);
+    }
+
+}
