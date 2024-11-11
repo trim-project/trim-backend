d8d661e827c4719b6a21307dadf7b090e7f9b8e4
hamlsy <dltmddud1122@naver.com>
2024-11-11 21:18:27 +0900

    feat(answer): Answer Entity 작성 #5

---
+++ src/main/java/com/trim/domain/answer/entity/Answer.java
@@ -0,0 +1,29 @@
+package com.trim.domain.answer.entity;
+
+import com.trim.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "writer_id")
+    private Member writer;
+
+
+}
