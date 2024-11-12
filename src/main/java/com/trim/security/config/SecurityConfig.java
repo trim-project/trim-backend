88c62f08ae56f1c866158d8d9809acf3e70c5a4f
hann <wjdgks1233@gmail.com>
2024-11-12 14:19:51 +0900

    feature : create securityConfig.class(config cors, security and auth) #6

---
+++ src/main/java/com/trim/security/config/SecurityConfig.java
@@ -0,0 +1,79 @@
+package com.trim.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import java.util.List;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        configureCorsAndSecurity(httpSecurity);
+        configureAuth(httpSecurity);
+        configureOAuth2(httpSecurity);
+        configureExceptionHandling(httpSecurity);
+        addFilter(httpSecurity);
+
+        return httpSecurity.build();
+    }
+
+
+    private void configureCorsAndSecurity(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .headers(
+                        httpSecurityHeadersConfigurer ->
+                                httpSecurityHeadersConfigurer.frameOptions(
+                                        HeadersConfigurer.FrameOptionsConfig::disable
+                                )
+                )
+                // stateless한 rest api 이므로 csrf 공격 옵션 비활성화
+                .csrf(AbstractHttpConfigurer::disable)
+                // 로그인 폼 기본 설정(커스텀 x)
+                .formLogin(Customizer.withDefaults())
+                // 팝업 로그인 x
+                .httpBasic(HttpBasicConfigurer::disable)
+                // 프론트 혹은 다른 외부 api와의 통신 허용
+                .cors(Customizer.withDefaults())
+                // rest api를 위한 stateless 설정
+                .sessionManagement(configurer -> configurer
+                        .sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS
+                        )
+                );
+    }
+    private void configureAuth(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(Customizer.withDefaults())
+                .authorizeHttpRequests(authorizeRequest -> {
+                    authorizeRequest
+                            .requestMatchers("/", "/.well-known/**", "/css/**",
+                                    "/*.ico", "/error", "/images/**").permitAll()
+                            .requestMatchers(permitAllRequest()).permitAll()        //비인증 api 허용 처리
+                            .anyRequest().permitAll();      //지정하지 않은 url의 경우 인증 처리
+                });
+    }
+
+    private RequestMatcher[] permitAllRequest() {
+        List<RequestMatcher> requestMatchers = List.of(
+                antMatcher(HttpMethod.GET, "/")     //list of 에 permit url 추가
+        );
+        return requestMatchers.toArray(RequestMatcher[]::new);
+    }
+}
