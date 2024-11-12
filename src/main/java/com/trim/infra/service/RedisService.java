faac4cce53814a0e489930386bfc70212d58d7e6
hann <wjdgks1233@gmail.com>
2024-11-12 16:26:40 +0900

    feature : compelete redisService #6

--- src/main/java/com/trim/infra/service/RedisService.java
+++ src/main/java/com/trim/infra/service/RedisService.java
@@ -1,4 +1,10 @@
 package com.trim.infra.service;
 
 public interface RedisService {
+
+    void setValueOfToken(String token, String username);
+
+    String getValueOfToken(String token);
+
+    void deleteToken(String token);
 }
--- src/main/java/com/trim/infra/service/RedisServiceImpl.java
+++ src/main/java/com/trim/infra/service/RedisServiceImpl.java
@@ -1,4 +1,37 @@
 package com.trim.infra.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class RedisServiceImpl implements RedisService{
+    private final RedisTemplate redisTemplate;
+
+    @Override
+    public void setValueOfToken(String token, String username) {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        values.set(token, username, Duration.ofDays(7));
+    }
+
+    @Override
+    public String getValueOfToken(String token) {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        return values.get(token);
+    }
+
+    @Override
+    public void deleteToken(String token) {
+        if (token != null && token.startsWith("Bearer ")) {
+            // "Bearer " 접두사 제거
+            token = token.substring(7);
+        }
+        redisTemplate.delete(token);
+    }
 }
