812067ddbbc3bceab8b1cbc783da0167c81a2042
hann <wjdgks1233@gmail.com>
2024-11-12 14:36:30 +0900

    feature : create TokenService and JwtToken Dto #6

---
+++ src/main/java/com/trim/security/service/TokenService.java
@@ -0,0 +1,19 @@
+package com.trim.security.service;
+
+import com.trim.security.dto.JwtToken;
+import org.springframework.security.core.Authentication;
+
+public interface TokenService {
+    JwtToken login(String kakaoEmail);
+    JwtToken issueTokens(String refreshToken);
+
+    JwtToken generateToken(Authentication authentication);
+
+    Authentication getAuthentication(String accessToken);
+
+    boolean validateToken(String token);
+
+    boolean logout(String refreshToken);
+
+    boolean existsRefreshToken(String refreshToken);
+}
