9e5a0e280a1ae911463b9197b2d1c2908fcd0db7
hann <wjdgks1233@gmail.com>
2024-11-12 14:27:38 +0900

    feature : implement UserDetail to Member entity and make role enum #6

--- src/main/java/com/trim/domain/member/entity/Member.java
+++ src/main/java/com/trim/domain/member/entity/Member.java
@@ -6,23 +6,28 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Entity
 @Getter
 @Table(name = "member")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member extends BaseTimeEntity {
+public class Member implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
     private Long id;
 
-//    @Enumerated(EnumType.STRING)
-//    private Role role;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     @Column(unique = true)
     private String username;
@@ -32,5 +37,27 @@ public class Member extends BaseTimeEntity {
     @OneToMany(mappedBy = "member")
     private List<Question> questionList = new ArrayList<>();
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority(this.role.getKey()));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;        //해당 항목 없음
+    }
+
+//    @OneToMany(mappedBy = "member")
+//    private List<Scrap> scrapList = new ArrayList<>();
+
+//    @OneToMany(mappedBy = "member")
+//    private List<AnswerLike> answerLikeList = new ArrayList<>();
+
+//    @OneToMany(mappedBy = "member")
+//    private List<Badge> badgeList = new ArrayList<>();
+
+//    @OneToOne(cascade = CascadeType.PERSIST)
+//    @JoinColumn(name = "avatar_id")
+//    private Avatar avatar;
 
 }
