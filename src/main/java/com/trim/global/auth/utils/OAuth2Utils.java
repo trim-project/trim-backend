a9f5a57c7beaf2b2f2cfef934e781cf8f3b7dc98
hamlsy <dltmddud1122@naver.com>
2024-11-23 23:23:28 +0900

    feat: add OAuth2Utils class #9

---
+++ src/main/java/com/trim/global/auth/utils/OAuth2Utils.java
@@ -0,0 +1,38 @@
+package com.trim.global.auth.utils;
+
+import com.trim.global.auth.Provider;
+import com.trim.global.auth.dto.GoogleResponse;
+import com.trim.global.auth.dto.KakaoResponse;
+import com.trim.global.auth.dto.NaverResponse;
+
+import java.util.Map;
+
+public class OAuth2Utils {
+
+    // registrationId에 따른 Provider 추출
+    public static Provider getProvider(String registrationId){
+
+        if(registrationId.equals("naver")){
+            return Provider.NAVER;
+        }
+        else if(registrationId.equals("google")){
+            return Provider.GOOGLE;
+        }
+        else if(registrationId.equals("kakao")){
+            return Provider.KAKAO;
+        }
+        return null;
+    }
+
+    public static Object getObject(Provider provider, Map<String, Object> attributes){
+        switch (provider){
+            case NAVER:
+                return new NaverResponse(attributes);
+            case GOOGLE:
+                return new GoogleResponse(attributes);
+            case KAKAO:
+                return new KakaoResponse(attributes);
+        }
+        return null;
+    }
+}
