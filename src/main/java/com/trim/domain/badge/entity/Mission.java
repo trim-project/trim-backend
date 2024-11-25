6d75eb750556cc74dee24667a1d3b960be0040a3
hann <wjdgks1233@gmail.com>
2024-11-25 23:02:53 +0900

    feature : create mission entity for member have it

---
+++ src/main/java/com/trim/domain/badge/entity/Mission.java
@@ -0,0 +1,31 @@
+package com.trim.domain.badge.entity;
+
+import com.trim.domain.auditing.entity.BaseTimeEntity;
+import com.trim.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Mission extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "mission_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "badge_id")
+    private Badge badge;
+}
