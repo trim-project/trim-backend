468e251da1df06579ee6497393262abf913537f8
hann <wjdgks1233@gmail.com>
2024-11-28 15:31:53 +0900

    feature : add RewardRepository #10

--- src/main/java/com/trim/domain/badge/repository/RewardRepository.java
+++ src/main/java/com/trim/domain/badge/repository/RewardRepository.java
@@ -1,8 +1,7 @@
 package com.trim.domain.badge.repository;
 
-import com.trim.domain.badge.entity.Mission;
 import com.trim.domain.badge.entity.Reward;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MissionRepository extends JpaRepository<Mission, Long> {
+public interface RewardRepository extends JpaRepository<Reward, Long> {
 }
