c0835a5d1ca140aae28d1ec6ffe5391d43250bd9
hann <wjdgks1233@gmail.com>
2024-12-02 14:46:23 +0900

    feature : create ExampleHolder for using swagger #11

--- src/main/java/com/trim/global/dto/ExampleHolder.java
+++ src/main/java/com/trim/global/dto/ExampleHolder.java
@@ -1,4 +1,13 @@
 package com.trim.global.dto;
 
+import io.swagger.v3.oas.models.examples.Example;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class ExampleHolder {
+    private Example holder;
+    private String name;
+    private int code;
 }
