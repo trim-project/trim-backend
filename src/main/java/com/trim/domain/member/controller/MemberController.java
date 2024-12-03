31fe328d76bc3ccd2620270184b0aec0e5a1aa60
hamlsy <dltmddud1122@naver.com>
2024-12-03 22:01:53 +0900

    feat: add test member controller #9

--- src/main/java/com/trim/domain/member/controller/MemberController.java
+++ src/main/java/com/trim/domain/member/controller/MemberController.java
@@ -4,16 +4,20 @@ import com.trim.domain.member.entity.Member;
 import com.trim.exception.payload.dto.ApiResponseDto;
 import com.trim.security.aop.CurrentUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/member")
 public class MemberController {
 
     /**
      *
      * 현재 로그인 중인 유저 정보 테스트
      */
+    @GetMapping("/info")
     public ApiResponseDto<?> currentUserInfo(@CurrentUser Member member){
         return ApiResponseDto.onSuccess(member);
     }
